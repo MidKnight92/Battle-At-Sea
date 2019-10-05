f0da79a5297eccd57ac7dbd7b8e433b190be5d10
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-05 12:17:00 -0500

    Added event listner to change Start Game text to Restart Game

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 console.log('Welcome');
 
+$('.startGameButtonStyle').on('click', (e) => {
+	$('.startGameButtonStyle').text('Restart Game');
+	$('.startGameButtonStyle').attr('id', 'restartBattle');
+	$('#restartBattle').removeClass('startGameButtonStyle');
+	$('#restartBattle').addClass('restartButtonStyle');	
+});
+
+$('.restartButtonStyle').on('click', (e) => {
+	// $('.restartButtonStyle').text('Start Game');
+	// $('#restartGameButton').addClass('class','startBattleButton');
+	// $('#restartGameButton').removeClass('restartButtonStyle')
+	// $('.startBattleButton').removeAttr('#restartGameButton');	
+});
 
 $('.player1-battle-grid').on('click', (e) => {
 	console.log(e.target.dataset);
