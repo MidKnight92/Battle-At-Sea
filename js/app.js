458cd4b29b9d53b3819644d882f992d098446abf
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-05 11:15:12 -0500

    Added a Start Game Button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 console.log('Welcome');
 
 
-$('.grid-container').on('click', (e) => {
+$('.player1-battle-grid').on('click', (e) => {
 	console.log(e.target.dataset);
 	console.log(e.target.dataset.whichSquareMoreWords);
-})
+});
+
+$('.player2-battle-grid').on('click', (e) => {
+	
+});
