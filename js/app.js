88e99c32c7bf597bf1a994727be4e770c0d3d895
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-12 00:41:39 -0500

    Added comments

--- js/app.js
+++ js/app.js
@@ -52,14 +52,6 @@ class Player {
 			RemaingFleet: []
 		}]
 	}
-
-	stillHasShips() {
-
-	}
-
-	placeShip() {
-
-	}
 }
 
 
@@ -68,7 +60,6 @@ const game = {
 	hitMiss: [],
 	p1: null,
 	p2: null,
-	whoseTurn: 'P1',
 	activePlayer: null,
 	inactivePlayer: null,
 	isBattling: false,
@@ -125,65 +116,63 @@ const game = {
 			RemaingFleet: []
 		}
 	],
+	//the start battle instantiates p1 and p2. It sets p1 to an activePlayer (the player that will start the picking ships) and p2 as an inactivePlayer  
 	startBattle(){
 		const P1 = new Player ();
 		const P2 = new Player ();
 		this.p1 = P1;
 		this.p2 = P2;
-		this.whoseTurn = "P1"
 		this.activePlayer = this.p1;
 		this.inactivePlayer = this.p2;
 	},
 	// later--post mvp--error check the pairs to make sure they're in a v or h line and close enough together
-	//.     0 is row (x) --- 1 is column (y)
-	// this takes the users click parses it and saves it into the cords array 
-	// grab the values from cords compare them and loop to give it the right letter
 	// later--post mvp--error check the pairs to make sure they're in a v or h line and close enough together
+
+	// this takes the users click parses it and saves it into the cords array 
+	// grab the values from cords compare them and loop to give it the right letter 
 	placeShips(userClicked){ 
-		
 		// grabs value sends it to be parsed then saves to userPicks array
 		let cords = this.parseCoordinates(userClicked);
 		this.userPicks.push(cords);
 		
-
+		//looks to see if user picked two different squares before running the function
 		if (this.userPicks.length === 2) {
 
-			//if the cols are even change y
+			//if the cols are even change rows
 			if (this.userPicks[0][0] === this.userPicks[1][0]) { 
-				//this makes the difference an absolute value
+				//this makes the difference between squares an absolute value
 				let diff_y = Math.abs(this.userPicks[1][1] - this.userPicks[0][1]);
 
-
-				for (let i = 0; i <= diff_y; i++) {
+					// this is incrementing up to the difference of the picks and adds the fleet starting at 0[this.placingShip] specifically the destroyer 
+					for (let i = 0; i <= diff_y; i++) {
 					this.fleet[this.placingShip];
-					// should figure out which ship to place
 					//horizontal placement below
+					//takes the first user pick and increment up to the second user pick
 					for (let i = this.userPicks[0][1]; i <= this.userPicks[1][1]; i++) {
-						const col = this.userPicks[0][0]
+						const col = this.userPicks[0][0] // board[col][row]
+						//add a letter representing the the ship in the fleet at the index of placingShip
 						this.activePlayer.board[col][i] = this.fleet[this.placingShip]['letter'];
-						
 					} 
 				} 
+				//if the rows are even change columns
 			} else if (this.userPicks[0][1] === this.userPicks[1][1]) {
 				let diff = Math.abs(this.userPicks[1][1] - this.userPicks[0][1])
 				for (let i = 0; i <= diff; i++) {
 					
 					this.fleet[this.placingShip];
 					// vertical placement below
+					//takes the first user pick and increment up to the second user pick
 					for (let i = this.userPicks[0][0]; i <= this.userPicks[1][0]; i++) {
 						const row = this.userPicks[0][1] // board[col][row]
+						//add a letter representing the the ship in the fleet at the index of placingShip
 						this.activePlayer.board[i][row] = this.fleet[this.placingShip]['letter'];
-						
 					}
 				}
 			}
-				// if 5 -- reset to 0
-				// 'done'
-				// later: change player
+			// once the 1 ship has been placed (above). check to see if all 5 ships have been placed if not increment up the placing ship and restart the placing process starting at the fleets next index. 
+			 //if all ships have been placed for p1 rest the placingShip index back to zero and switch to p2 repeat this process once p2 is done placing ships change the state of the battling to true and this method will stop running because of the setOrBattle method and battling will begin 
 			if (this.placingShip === 4) {
-				
 				if (this.activePlayer === this.p2 && !this.isBattling) {
-					
 					this.isBattling = true;
 				}
 				this.placingShip = 0;
@@ -203,20 +192,17 @@ const game = {
 		const y = parseInt(arr1[2]);
 		return [x , y]; 
 	},										
-	// remove alert // notification to display names of ships and lengths and ship placement rules
+	// notification to display names of ships and lengths and ship placement rules
 	// placeFleet(player){
 	// 	alert(`${player} place your fleet:\n-The ${this.fleet[0]['typeOfShip']} has a length of ${this.fleet[0]['length']}.\n-The ${this.fleet[1]['typeOfShip']} has a length of ${this.fleet[1]['length']}.\n-The ${this.fleet[2]['typeOfShip']} has a length of ${this.fleet[2]['length']}.\n-The ${this.fleet[3]['typeOfShip']} has a length of ${this.fleet[3]['length']}.\n-The ${this.fleet[4]['typeOfShip']} has a length of ${this.fleet[4]['length']}.\nNote: Ships can be placed horizontally or vertically and they can not touch.`);
 	// },
 	switchPlayers() {
-		
 		if (this.activePlayer === this.p1) {
-			
 			this.activePlayer = this.p2;
 			this.inactivePlayer = this.p1;
 		} else {
 			this.activePlayer = this.p1;
 			this.inactivePlayer = this.p2;
-			
 		}
 	},
 	//create a function that will change the html to show ships
@@ -293,9 +279,6 @@ const game = {
 	// 	$('#restartBattle').addClass('restartButtonStyle');
 	// },
 	hitOrMiss(clickInfo){ 
-		// let cords = this.parseCoordinates(clickInfo);
-		// this.hitMiss.push(cords);
-		
 		const arr1 = clickInfo.square.split('');
 		const x = parseInt(arr1[0]);
 		const y = parseInt(arr1[2]);
@@ -304,22 +287,24 @@ const game = {
  		// if (it's p1's turn and they clicked on p2's board) or (if it's p2's turn and they clicked on p1's board)
  			
 	 		if ((this.activePlayer === this.p1 && clickInfo.player === 'p2') || (this.activePlayer === this.p2 &&  clickInfo.player === 'p1')) {
-	 			//check stuff
+	 			//checks inactive player board
 	 			if (this.inactivePlayer.board[x][y] == 0){
-		 			
+		 			//if that square does not have a ship it gives it the class of miss and switches player
 		 			$(`.grid-item[data-player=${clickInfo.player}][data-square=${clickInfo.square}]`).addClass('miss');
-		 			
 		 			this.switchPlayers();
+		 			//if there is a ship add the class as hit and change the lowercase letter to an uppercase letter so it can not be clicked again and registered as a hit
+		 			//calls the check for winners method
 	 			} else if ((this.inactivePlayer.board[x][y] === 'd') || (this.inactivePlayer.board[x][y] === 'c') || (this.inactivePlayer.board[x][y] === 's') || (this.inactivePlayer.board[x][y] === 'b') || (this.inactivePlayer.board[x][y] === 'a')) {
 		 			$(`.grid-item[data-player=${clickInfo.player}][data-square=${clickInfo.square}]`).addClass('hit');
 		 			let letter = this.inactivePlayer.board[x][y].toUpperCase();
 		 			this.inactivePlayer.board[x].splice(y, 1, letter);
 		 			this.checkWinners();
-		 			//this.switchPlayers();
+		 			// if this square has already been hit click a different square
 		 		} else if ((this.inactivePlayer.board[x][y] === 'D') || (this.inactivePlayer.board[x][y] === 'C') || (this.inactivePlayer.board[x][y] === 'S') || (this.inactivePlayer.board[x][y] === 'B') || (this.inactivePlayer.board[x][y] === 'A')) {
 		 			alert(`Pick a different square.`);
 		 		}
 	 		} else {
+	 			//if the active player accidentally clicks on their own board it alerts the user it is the wrong board
 	 			alert(`not your turn`);
 	 		}
 	 		
@@ -340,23 +325,24 @@ const game = {
 		$('#p2HitsStats').text(`Hits: ${player2.hits}`);
 		$('#p2MissesStats').text(`Misses: ${player2.misses}`);
 	},
+	//assumes there is a winner
+		// looks through every element in the inactivePlayer board 
+			//if the elements are strings and are uppercased and is not just a single element change activeplayer to false 
 	checkWinners(){
-		console.log("checking winners");
 		let activePlayerWon = true;
 		for (let y = 0; y < this.inactivePlayer.board.length; y++) {
 			for (let x = 0; x < this.inactivePlayer.board.length; x++) {
 				const singleElement = this.inactivePlayer.board[y][x]
 				if (typeof singleElement === "string" && singleElement.toUpperCase() !== singleElement) {
 					activePlayerWon = false;
-					console.log('checked for upppers');
 				}
 			}
 		} 
+		// if there were lowercase letter it will say !true === false  which is what we are looking for in the condition we are look for it runs the switch 
+		// !false === true ignores condition it declares winner
 		if(!activePlayerWon) {
-			console.log("about to switch players - nope -");
 			this.switchPlayers();
 		} else {
-			console.log('you won');
 			$('.battle-grid').off('click', (e) => {
 			this.setOrBattle(e.target.dataset);
 		});
@@ -374,7 +360,7 @@ const game = {
 
 // ------------------- Event Listners --------------------- //
 $('.startGameButtonStyle').on('click', (e) => {
-	// console.log(game.board);
+
 	// game.changeStartGameText();
 	game.showShips();
 });
@@ -387,13 +373,8 @@ $('.battle-grid').on('click', (e) => {
 	// or sometimes do ship placement
 	// game.parseCoordinates(e.target.dataset);
 	game.setOrBattle(e.target.dataset);
-	// console.log(e.target.dataset);
 });
 
 $('#instructions').on('click', (e) => {
 	game.displayInstructions();
 });
-
-
-//$('.grid-item[data-square=0-0]')
-//$('.player1-battle-grid .grid-item[data-square=0-0]')
